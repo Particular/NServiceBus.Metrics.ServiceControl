6726b4b298fc24e449cb5eb1ffc3e6c7c109d218
Szymon Kulec <scooletz@gmail.com>
2017-09-01 13:48:14 +0200

    More v5

---
+++ src/NServiceBus.Metrics.ServiceControl/Buffers.cs
@@ -0,0 +1,37 @@
+﻿namespace NServiceBus.Metrics.ServiceControl
+{
+    using System;
+    using System.Threading;
+    using global::ServiceControl.Monitoring.Data;
+
+    class Buffers
+    {
+        public readonly RingBuffer ProcessingTime = new RingBuffer();
+        public readonly TaggedLongValueWriterV1 Writer = new TaggedLongValueWriterV1();
+
+        void Write(long value, string tag, RingBuffer buffer)
+        {
+            const int maxAttempts = 10;
+
+            if (buffer.TryWrite(value, Writer.GetTagId(tag)))
+            {
+                return;
+            }
+
+            var spin = new SpinWait();
+            for (var i = 0; i < maxAttempts; i++)
+            {
+                spin.SpinOnce();
+                if (buffer.TryWrite(value, Writer.GetTagId(tag)))
+                {
+                    return;
+                }
+            }
+        }
+
+        public void ReportProcessingTime(TimeSpan value, string messageType)
+        {
+            Write((long)value.TotalMilliseconds, messageType, ProcessingTime);
+        }
+    }
+}
---
+++ src/NServiceBus.Metrics.ServiceControl/MetricsOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace NServiceBus.Metrics.ServiceControl
+{
+    using System;
+
+    class MetricsOptions
+    {
+        public void SendMetricDataToServiceControl(string serviceControlMetricsAddress, TimeSpan interval, string instanceId = null)
+        {
+            ServiceControlMetricsAddress = serviceControlMetricsAddress;
+            ServiceControlReportingInterval = interval;
+            endpointInstanceIdOverride = instanceId;
+        }
+
+        internal string ServiceControlMetricsAddress;
+        internal TimeSpan ServiceControlReportingInterval;
+        string endpointInstanceIdOverride;
+
+        internal bool TryGetValidEndpointInstanceIdOverride(out string instanceId)
+        {
+            if (string.IsNullOrEmpty(endpointInstanceIdOverride) == false)
+            {
+                instanceId = endpointInstanceIdOverride;
+                return true;
+            }
+
+            instanceId = null;
+            return false;
+        }
+    }
+}
