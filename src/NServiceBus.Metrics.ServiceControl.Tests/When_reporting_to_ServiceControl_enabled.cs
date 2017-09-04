ef46b7bfb7052a78ae7b418f519a24e697ad204d
Szymon Kulec <scooletz@gmail.com>
2017-09-04 11:09:29 +0200

    Working test with proper assertions.

--- src/NServiceBus.Metrics.ServiceControl.Tests/When_reporting_to_ServiceControl_enabled.cs
+++ src/NServiceBus.Metrics.ServiceControl.Tests/When_reporting_to_ServiceControl_enabled.cs
@@ -1,6 +1,9 @@
 ﻿namespace NServiceBus.Metrics.ServiceControl.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
     using System.Threading;
     using AcceptanceTesting;
     using AcceptanceTesting.Customization;
@@ -9,13 +12,12 @@
     using Pipeline;
     using Pipeline.Contexts;
 
-    public class When_sending_to_ServiceControl_Monitoring : NServiceBusAcceptanceTest
+    public class When_reporting_to_ServiceControl_enabled : NServiceBusAcceptanceTest
     {
-        static readonly string ServiceControlMetricsAddress = Conventions.EndpointNamingConvention(typeof(MonitoringMock));
         const string CustomInstanceId = "my-custom-instance";
 
         [Test]
-        public void Should_report()
+        public void Should_report_properly_formatted_message()
         {
             var context = new Context();
 
@@ -28,29 +30,49 @@
                     }
                 }))
                 .WithEndpoint<MonitoringMock>()
-                .Done(c => c.GotReport)
+                .Done(c => c.Body != null)
                 .Run();
 
-            Assert.True(context.GotReport, "Should have received report");
+            Assert.AreEqual("ProcessingTime", context.Headers["NServiceBus.Metric.Type"]);
+            Assert.AreEqual(CustomInstanceId, context.Headers["NServiceBus.Metric.InstanceId"]);
+            Assert.AreEqual("TaggedLongValueWriterOccurrence", context.Headers["NServiceBus.ContentType"]);
+
+            // dummy assert for containing the name of message in the message body
+            var fullyQualifiedName = new UTF8Encoding(false).GetBytes(typeof(MyMessage).AssemblyQualifiedName);
+            Assert.True(ContainsPattern(context.Body, fullyQualifiedName), "The message should contain the fully qualified name of the reported message");
+        }
+
+        static bool ContainsPattern(byte[] source, byte[] pattern)
+        {
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (source.Skip(i).Take(pattern.Length).SequenceEqual(pattern))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public class Context : ScenarioContext
         {
-            public bool GotReport { get; set; }
+            public Dictionary<string, string> Headers { get; set; }
+            public byte[] Body { get; set; }
         }
 
         public class Sender : EndpointConfigurationBuilder
         {
             public Sender()
             {
-                EndpointSetup<DefaultServer>(cfg => cfg.EnableReporting().SendMetricDataToServiceControl(ServiceControlMetricsAddress, CustomInstanceId));
+                EndpointSetup<DefaultServer>(cfg => cfg.EnableReporting().SendMetricDataToServiceControl(Conventions.EndpointNamingConvention(typeof(MonitoringMock)), CustomInstanceId));
             }
 
             public class MyMessageHandler : IHandleMessages<MyMessage>
             {
                 public void Handle(MyMessage message)
                 {
-                    Thread.Sleep(1000);
+                    Thread.Sleep(100);
                 }
             }
         }
@@ -76,13 +98,13 @@
 
                 public void Invoke(IncomingContext context, Action next)
                 {
-                    //var transportMessage = context.PhysicalMessage;
-                    Context.GotReport = true;
+                    Context.Headers = context.PhysicalMessage.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    Context.Body = context.PhysicalMessage.Body.ToArray();
                 }
             }
         }
 
         public class MyMessage : IMessage
-        {}
+        { }
     }
 }
