40a3487666eaa71378338633205c609cd8356e5c
Szymon Kulec <scooletz@gmail.com>
2017-09-26 13:55:40 +0200

    Custom approvals

--- src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.cs
+++ src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using ApprovalTests;
+using ApprovalTests.Core;
 using NUnit.Framework;
 using PublicApiGenerator;
 
@@ -14,7 +17,38 @@ public class APIApprovals
     {
         var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Metrics.ServiceControl.dll");
         var assembly = Assembly.LoadFile(combine);
-        var publicApi = ApiGenerator.GeneratePublicApi(assembly);
-        Approvals.Verify(publicApi);
+        var publicApi = Filter(ApiGenerator.GeneratePublicApi(assembly));
+        Approvals.Verify(BuildWriter(publicApi));
+    }
+
+    static IApprovalWriter BuildWriter(string api,[CallerFilePath] string path = null)
+    {
+        var directory = Path.GetDirectoryName(path);
+        return new LocalApprovalTextWriter(api, "cs", directory);
+    }
+
+    string Filter(string text)
+    {
+        return string.Join(Environment.NewLine, text.Split(new[]
+            {
+                Environment.NewLine
+            }, StringSplitOptions.RemoveEmptyEntries)
+            .Where(l => !l.StartsWith("[assembly: System.Runtime.Versioning"))
+            .Where(l => !string.IsNullOrWhiteSpace(l))
+        );
+    }
+
+    class LocalApprovalTextWriter : ApprovalTextWriter
+    {
+        readonly string directory;
+
+        public LocalApprovalTextWriter(string data, string extensionWithoutDot, string directory) 
+            : base(data, extensionWithoutDot)
+        {
+            this.directory = directory;
+        }
+
+        public override string GetApprovalFilename(string basename) => Path.Combine(directory, base.GetApprovalFilename(basename));
+        public override string GetReceivedFilename(string basename) => Path.Combine(directory, base.GetReceivedFilename(basename));
     }
 }
