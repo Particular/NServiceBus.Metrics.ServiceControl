befc3a8a06b835249d5771051820a999d203f200
Szymon Kulec <scooletz@gmail.com>
2017-09-26 14:00:05 +0200

    Approvals added

--- src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.Approve.approved.cs
+++ src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.Approve.approved.cs
@@ -1,1 +1,7 @@
-﻿ 
+﻿namespace NServiceBus
+{
+    public class static MetricsOptionsExtensions
+    {
+        public static void SendMetricDataToServiceControl(this NServiceBus.MetricsOptions options, string serviceControlMetricsAddress, System.TimeSpan interval, string instanceId = null) { }
+    }
+}
