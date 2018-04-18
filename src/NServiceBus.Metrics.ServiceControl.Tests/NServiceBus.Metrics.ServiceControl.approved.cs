b68399760c3867c85ff019fada99b92db7afa783
Mike Minutillo <mike.minutillo@particular.net>
2018-04-18 16:46:03 +0800

    Approve API change

--- src/NServiceBus.Metrics.ServiceControl.Tests/NServiceBus.Metrics.ServiceControl.approved.cs
+++ src/NServiceBus.Metrics.ServiceControl.Tests/NServiceBus.Metrics.ServiceControl.approved.cs
@@ -5,4 +5,8 @@
         public static void SendMetricDataToServiceControl(this NServiceBus.BusConfiguration busConfiguration, string serviceControlMetricsAddress, string instanceId = null) { }
         public static void SetServiceControlMetricsMessageTTBR(this NServiceBus.BusConfiguration busConfiguration, System.TimeSpan timeToBeReceived) { }
     }
+    public interface IReportNativeQueueLength
+    {
+        void ReportQueueLength(string physicalQueueName, long queueLength);
+    }
 }
