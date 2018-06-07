b1b2364c3aed405a645af61322d9cbb83ba3c279
Tomasz Masternak <tomasz.masternak@particular.net>
2018-06-07 12:14:27 +0200

    fixing att

--- src/NServiceBus.Metrics.ServiceControl.AcceptanceTests/Tests/EndpointMetadataReport.cs
+++ src/NServiceBus.Metrics.ServiceControl.AcceptanceTests/Tests/EndpointMetadataReport.cs
@@ -3,6 +3,6 @@
     public class EndpointMetadataReport : IMessage
     {
         public string LocalAddress { get; set; }
-        public int Version { get; set; }
+        public int PluginVersion { get; set; }
     }
 }
