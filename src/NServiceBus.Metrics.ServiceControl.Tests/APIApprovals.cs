a3723a2027e880a1c78c2bc3379aa6061035d20f
Ramon Smits <ramon.smits@gmail.com>
2018-06-27 10:04:43 +0200

    API Approvals update

--- src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.cs
+++ src/NServiceBus.Metrics.ServiceControl.Tests/APIApprovals.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using ApprovalTests;
-using ApprovalTests.Core;
+﻿using System.Runtime.CompilerServices;
+using NServiceBus;
+using NServiceBus.Metrics.ServiceControl.Msmq.Tests;
 using NUnit.Framework;
 using PublicApiGenerator;
 
@@ -15,40 +11,7 @@ public class APIApprovals
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void Approve()
     {
-        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Metrics.ServiceControl.dll");
-        var assembly = Assembly.LoadFile(combine);
-        var publicApi = Filter(ApiGenerator.GeneratePublicApi(assembly));
-        Approvals.Verify(BuildWriter(publicApi));
-    }
-
-    static IApprovalWriter BuildWriter(string api,[CallerFilePath] string path = null)
-    {
-        var directory = Path.GetDirectoryName(path);
-        return new LocalApprovalTextWriter(api, "cs", directory);
-    }
-
-    string Filter(string text)
-    {
-        return string.Join(Environment.NewLine, text.Split(new[]
-            {
-                Environment.NewLine
-            }, StringSplitOptions.RemoveEmptyEntries)
-            .Where(l => !l.StartsWith("[assembly: System.Runtime.Versioning"))
-            .Where(l => !string.IsNullOrWhiteSpace(l))
-        );
-    }
-
-    class LocalApprovalTextWriter : ApprovalTextWriter
-    {
-        readonly string directory;
-
-        public LocalApprovalTextWriter(string data, string extensionWithoutDot, string directory) 
-            : base(data, extensionWithoutDot)
-        {
-            this.directory = directory;
-        }
-
-        public override string GetApprovalFilename(string basename) => Path.Combine(directory, base.GetApprovalFilename(basename));
-        public override string GetReceivedFilename(string basename) => Path.Combine(directory, base.GetReceivedFilename(basename));
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(MetricsOptionsExtensions).Assembly);
+        TestApprover.Verify(publicApi);
     }
 }
