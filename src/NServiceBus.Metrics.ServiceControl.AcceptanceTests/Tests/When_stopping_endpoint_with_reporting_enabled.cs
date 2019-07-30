27f4c552c3c52d4244a3c5a223f0243318a31112
danielmarbach <daniel.marbach@openplace.net>
2019-07-30 10:39:05 +0200

    Merge pull request #63 from Particular/cancel-loop
    Cancel reporting loop

---
+++ src/NServiceBus.Metrics.ServiceControl.AcceptanceTests/Tests/When_stopping_endpoint_with_reporting_enabled.cs
@@ -0,0 +1,65 @@
+namespace NServiceBus.Metrics.AcceptanceTests
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_stopping_endpoint_with_reporting_enabled : NServiceBusAcceptanceTest
+    {
+        static string MonitoringSpyAddress => Conventions.EndpointNamingConvention(typeof(MonitoringSpy));
+        static TimeSpan SendInterval = TimeSpan.FromSeconds(30);
+
+        [Test]
+        public async Task Should_not_delay_endpoint_stop()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            
+            await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<Sender>()
+                .WithEndpoint<MonitoringSpy>()
+                .Done(c => c.EndpointsStarted)
+                .Run()
+                .ConfigureAwait(false);
+            
+            stopWatch.Stop();
+            
+            Assert.That(stopWatch.Elapsed, Is.LessThan(SendInterval));
+        }
+        
+        class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.EnableMetrics().SendMetricDataToServiceControl(MonitoringSpyAddress, SendInterval);
+                });
+            }
+        }
+
+        class MonitoringSpy : EndpointConfigurationBuilder
+        {
+            public MonitoringSpy()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.UseSerialization<JsonSerializer>();
+                    c.LimitMessageProcessingConcurrencyTo(1);
+                }).IncludeType<EndpointMetadataReport>();
+            }
+
+            public class MetricHandler : IHandleMessages<EndpointMetadataReport>
+            {
+                public Task Handle(EndpointMetadataReport message, IMessageHandlerContext context)
+                {
+                    return Task.FromResult(0);
+                }
+            }
+        }
+    }
+}
